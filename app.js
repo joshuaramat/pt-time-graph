a3290516239c948c054ff068856b8d8fd1d4f0b8
Joshua Ramat <ramatjoshua@gmail.com>
2023-04-27 16:19:50 -0700

    configured app.js with body-parser and cors

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 
-// configure body-parser package
-// configure cors
-// import scheduler routes
+const schedulerRoutes = require('./routes/scheduler');
 
 const PORT = 3301;
 
 app.get('/', function(req, res, next) {
   res.send('Hello World')
 })
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended:true }));
+
+app.use(cors());
+
+app.use('/scheduler', schedulerRoutes);
 
 app.listen(PORT, () => {
   console.log(`Serves is listening on port ${PORT}`);
